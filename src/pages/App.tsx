4448abd849573ae8f2fe84d75d818d8d42bbad33
Leon <leon.nl0107@gmail.com>
2025-09-29 18:41:59 -0700

    added map in homepage

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,23 +1,58 @@
 import { useState } from 'react'
 import './App.css'
+import { GoogleMap, useJsApiLoader, Marker, InfoWindow } from '@react-google-maps/api';
 
 export default function App() {
-  const [count, setCount] = useState(0)
+  const [selectedMarker, setSelectedMarker] = useState<number | null>(null);
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY
+  });
+
+  type MarkerType = {
+    geocode: { lat: number; lng: number };
+    popUp: string;
+  }
+
+  const markers: MarkerType[] = [
+    {
+      geocode: { lat: 49.24, lng: -123.05 },
+      popUp: "marker1"
+    },
+    {
+      geocode: { lat: 49.23, lng: -123.05 },
+      popUp: "marker2"
+    }
+  ]
+
+  const mapContainerStyle = {
+    height: "100vh",
+    width: "100vw"
+  };
+
+  const center = { lat: 49.24, lng: -123.05 }
+if (!isLoaded) return <div>Loading map...</div>;
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <GoogleMap
+          mapContainerStyle={mapContainerStyle}
+          center={center}
+          zoom={13}
+        >
+          {markers.map((marker, index) => (
+            <Marker
+              key={index}
+              position={marker.geocode}
+              onClick={() => setSelectedMarker(index)}
+            >
+              {selectedMarker === index && (
+                <InfoWindow onCloseClick={() => setSelectedMarker(null)}>
+                  <div>{marker.popUp}</div>
+                </InfoWindow>
+              )}
+            </Marker>
+          ))}
+        </GoogleMap>
     </>
   )
 }
