624ba9cb895c73e45519272a3bae9845bc04a6b9
Tanvir Waseer <twaseer@protonmail.com>
2025-10-22 01:39:04 -0700

    add rating-section div inside WalkerProfile

--- src/pages/owner/walkerProfile.tsx
+++ src/pages/owner/walkerProfile.tsx
@@ -1,5 +1,6 @@
 import { useLocation, useNavigate } from 'react-router';
 import profileBanner from '../../assets/profile_banner.png';
+import { Star, BadgeCheck } from "lucide-react";
 import '../App.css'
 
 export default function WalkerProfile() {
@@ -14,14 +15,27 @@ export default function WalkerProfile() {
   return (
     <div className="walker-profile">
       <img src={profileBanner} alt={user.name} className="profile-banner" />
-      
+
       <div className="profile-content">
         <div className="profile-header">
           <h1>{user.name}</h1>
-          <div className="rating">
-            <span>★ 4.53 (12)</span>
+
+        </div>
+
+        <div className="flex justify-between rating-section">
+          <div className="flex">
+            <div><Star /></div>
+            <div className="ml-1">4.53 (12)</div>
+          </div>
+
+          <div className="flex">
+            <div><BadgeCheck /></div>
+            <div className="ml-1">Verified Walker</div>
           </div>
         </div>
+
+        <hr className="mt-5"></hr>
+
 
         <div className="profile-details">
           <p>${user.price || '30'} per dog</p>
