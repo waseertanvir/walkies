729cd24c4cee5b5ceaf7e93e7e46f63fa51732c8
Tanvir Waseer <twaseer@protonmail.com>
2025-10-22 02:08:54 -0700

    add file RequestWalk.tsx

---
+++ src/pages/owner/RequestWalk.tsx
@@ -0,0 +1,20 @@
+import { useLocation, useNavigate } from 'react-router';
+import profileBanner from '../../assets/profile_banner.png';
+import { Star, BadgeCheck } from "lucide-react";
+import '../App.css'
+
+export default function RequestWalk() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const user = location.state?.user;
+
+  if (!user) {
+    return <div>User not found</div>;
+  }
+
+  return (
+    <div className="request-walk">
+      
+    </div>
+  );
+}
