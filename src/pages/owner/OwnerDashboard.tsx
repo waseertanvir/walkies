82bf4c7e809410041efbbdf56fe8ba39bd737aca
Tanvir Waseer <twaseer@protonmail.com>
2025-11-04 21:16:47 -0800

    add waiting for walker div in OwnerDashboard.tsx

--- src/pages/owner/OwnerDashboard.tsx
+++ src/pages/owner/OwnerDashboard.tsx
@@ -307,24 +307,50 @@ export default function App() {
           </div>
         )}
 
-        {!clickedUser && state === 'BROADCAST' && (
+        {!clickedUser && state === 'WAITING_FOR_WALKER' && (
           <div className="absolute bottom-0 w-full h-auto rounded-t-xl rounded-b-none bg-wsage p-5">
-            <div className="flex items-center w-full">
-              <p className="flex-1">Waiting for walker</p>
+            <div className='grid items-center justify-center h-full w-full'>
+              <img
+                className="
+                relative
+                left-1/2
+                -top-20
+                transform -translate-x-1/2
+                max-w-[125px] max-h-[125px] w-full h-auto
+                rounded-full border-4 border-yellow-400 object-cover
+                "
+                src="https://m.gettywallpapers.com/wp-content/uploads/2023/09/Grand-Theft-Auto-5-Profile-Picture.jpg"
+                alt="Franklin with Chop."
+              />
 
               <img
                 src={logo}
                 alt="Logo"
-                className="max-w-[10%] h-auto ml-auto"
+                className="
+                absolute          
+                top-0 right-0     
+                m-5     
+                max-h-10         
+                max-w-10      
+                h-auto
+                "
               />
-            </div>
 
-            <div className='w-full'>
-              <Loader />
+              <p className="absolute inset-x-0 top-20 text-center text-yellow-500 font-bold text-1xl">
+                Franklin
+              </p>
+
+              <div className='relative w-full -top-5'>
+                <Loader />
+              </div>
+
+              <div className="bg-orange-500 hover:bg-orange-600 text-white font-medium py-2 px-4 rounded-md text-center">
+                REQUESTED
+              </div>
             </div>
           </div>
-        )}
 
+        )}
 
       </APIProvider>
     </ProtectedRoute>
