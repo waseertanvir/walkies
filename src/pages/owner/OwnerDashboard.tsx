aed65a96c053b0d0758211e863d80b31bac25e46
Tanvir Waseer <twaseer@protonmail.com>
2025-11-09 16:30:19 -0800

    set schedule on clike to /requests/new

--- src/pages/owner/OwnerDashboard.tsx
+++ src/pages/owner/OwnerDashboard.tsx
@@ -300,7 +300,7 @@ export default function App() {
               <button className="p-4 mr-2 rounded-3xl bg-worange" onClick={() => navigate("/owner/broadcast")}>
                 Broadcast
               </button>
-              <button className="p-4 ml-2 rounded-3xl bg-worange" onClick={() => navigate("/owner/schedule")}>
+              <button className="p-4 ml-2 rounded-3xl bg-worange" onClick={() => navigate("/requests/new")}>
                 Schedule
               </button>
             </div>
