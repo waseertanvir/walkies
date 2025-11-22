98fa607a3f20b602c1ceedbd251fc578a2b0b109
Dilpreet Singh Sohi <47670785+sohidilpreet@users.noreply.github.com>
2025-11-21 16:44:46 -0800

    server issue

--- src/services/Server.tsx
+++ src/services/Server.tsx
@@ -3,7 +3,7 @@ export async function findProfileById(uuid: string, token: string) {
       console.log("REST call failure. Token is null!");
     }
 
-    const responseBody = await fetch('https://157.230.155.46:49152/walkies-profile-service/profile/'+uuid, {
+    const responseBody = await fetch('http://157.230.155.46:49152/walkies-profile-service/profile/'+uuid, {
       method: "GET",
       headers: {"Authorization": `Bearer ${token}`}
     })
